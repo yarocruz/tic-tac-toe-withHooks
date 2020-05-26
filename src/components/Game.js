39a0f0b77b236d1fb219f135e30ee778a2aff2ad
yarocruz <yarocruz@gmail.com>
2020-05-26 14:37:04 -0400

    setup to deploy to gh-pages

--- src/components/Game.js
+++ src/components/Game.js
@@ -85,16 +85,18 @@ export default function Game() {
     }
 
     return (
-        <div className="game">
-            <div className="game-board">
-                <Board
-                    squares={current.squares}
-                    onClick={(i) => handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
+        <div style={{ maxWidth: '960px', margin: '0 auto'}}>
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
             </div>
         </div>
     )
